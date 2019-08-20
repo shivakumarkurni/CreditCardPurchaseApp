6e4854b7db5291a9e8517d1a52736596845ad210
User1 <User1@LP-9TY9TS1.HCLT.CORP.HCL.IN>
2019-08-20 19:08:24 +0530

    Added test case for Controller

--- src/test/java/com/credit/otpVerify/TestCreditCardController.java
+++ src/test/java/com/credit/otpVerify/TestCreditCardController.java
@@ -3,13 +3,13 @@ package com.credit.otpVerify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,8 +18,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.credit.controller.CreditCardController;
+import com.credit.dto.CreditCardInputDto;
+import com.credit.dto.CreditCardOtpVerificationInput;
 import com.credit.dto.OtpDto;
+import com.credit.entity.CreditCard;
 import com.credit.service.VerifyServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -50,4 +54,45 @@ public class TestCreditCardController {
 		mockMvc.perform(post("/creditcard/verification").contentType(MediaType.APPLICATION_JSON).content(jsonObject)).andExpect(status().isOk());
 	}
 	
+	@Test
+	public void testCardCheck() throws Exception {
+		CreditCardInputDto creditCardInputDto = new CreditCardInputDto();
+		creditCardInputDto.setAmount(4000);
+		creditCardInputDto.setCardNumber(1234567891234L);
+		creditCardInputDto.setCvv(1234);
+		creditCardInputDto.setExpiryMoth(11);
+		creditCardInputDto.setExpiryYear(21);
+		creditCardInputDto.setName("LAXMAN VERMA");
+		creditCardInputDto.setUserId(101);
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonObject = objectMapper.writeValueAsString(creditCardInputDto);
+		mockMvc.perform(post("/creditcard").contentType(MediaType.APPLICATION_JSON).content(jsonObject)).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testCardCheckOtpVerification() throws Exception {
+		CreditCardOtpVerificationInput creditCardOtpVerificationInput = new CreditCardOtpVerificationInput();
+		creditCardOtpVerificationInput.setOtp(1234);
+		creditCardOtpVerificationInput.setTransactionId(1010);
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonObject = objectMapper.writeValueAsString(creditCardOtpVerificationInput);
+		mockMvc.perform(post("/creditcard/verification").contentType(MediaType.APPLICATION_JSON).content(jsonObject)).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testCardSave() throws Exception {
+		CreditCard creditCard = new CreditCard();
+		creditCard.setAvailableBalance(6000);
+		creditCard.setAvailableLimit(10000);
+		creditCard.setCardId(101);
+		creditCard.setCardNumber(123456788912L);
+		creditCard.setCvv(123);
+		creditCard.setExpireDate(LocalDate.of(2019, 12, 23));
+		creditCard.setUserId(131);
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonObject = objectMapper.writeValueAsString(creditCard);
+		mockMvc.perform(post("/creditcard/cardSave").contentType(MediaType.APPLICATION_JSON).content(jsonObject)).andExpect(status().isOk());
+		
+	}
 }
